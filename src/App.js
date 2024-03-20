6d688516bbfe599ae25aa2fa09448b5ea073a14e
yunchipang <yunchipang@gmail.com>
2024-03-20 13:36:31 -0700

    connect to springboot backend locally

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Update the URL to match your Spring Boot app's URL if it's not running on localhost:8080
+    fetch("http://localhost:8080/")
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("There was an error!", error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message ? message : "Loading..."}</p>
       </header>
     </div>
   );
